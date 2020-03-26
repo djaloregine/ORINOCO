6ab6e0d818c465c58016fd55267dd7f8ddb2bd01
Regine Djalo <djalo50@gmail.com>
2020-03-26 18:32:56 +0100

    avec fetch

--- JS/main.js
+++ JS/main.js
@@ -20,52 +20,37 @@ axios.get("http://localhost:3000/api/teddies").then(response => {
 */
 
 
-const getTeddies = async function () {
-  console.log(document.querySelector(".list__produits"));
-  let response = await fetch("http://localhost:3000/api/teddies");
-  if (response.ok) {
-    let data = await response.json();
-    console.log(data);
-    // data = nom de mon tableau 
-    for (let i = 0; i < data.length; i++) {
-      let nameNounours = data[i].name;
-      let imgNounours = data[i].imageUrl;
+fetch("http://localhost:3000/api/teddies")
+  .then(response => response.json().then(data => {
+    if (response.ok) {
+      console.log(data);
+      // data = nom de mon tableau 
+      for (let i = 0; i < data.length; i++) {
+        let nameNounours = data[i].name;
+        let imgNounours = data[i].imageUrl;
 
 
-      liNounours = document.createElement("li");
-      liNounours.classList = "ours";
-      aNounours = document.createElement("a");
-      aNounours.href = data[i]._id;
-      aNounours.classList = "aOurs"
-      pNounours = document.createElement('p');
-      pNounours.classList = "pOurs"
-      pNounours.textContent = nameNounours;
+        liNounours = document.createElement("li");
+        liNounours.classList = "ours";
+        aNounours = document.createElement("a");
+        aNounours.href = data[i]._id;
+        aNounours.classList = "aOurs"
+        pNounours = document.createElement('p');
+        pNounours.classList = "pOurs"
+        pNounours.textContent = nameNounours;
 
-      imgNounours = document.createElement("img");
-      imgNounours.classList = "imgOurs";
-      imgNounours.src = data[i].imageUrl;
+        imgNounours = document.createElement("img");
+        imgNounours.classList = "imgOurs";
+        imgNounours.src = data[i].imageUrl;
 
-      ulNounours = document.getElementById("list__produits");
-      liNounours.appendChild(aNounours);
-      aNounours.appendChild(pNounours);
-      liNounours.appendChild(imgNounours);
-      ulNounours.appendChild(liNounours);
+        ulNounours = document.getElementById("list__produits");
+        liNounours.appendChild(aNounours);
+        aNounours.appendChild(pNounours);
+        liNounours.appendChild(imgNounours);
+        ulNounours.appendChild(liNounours);
+      }
 
-
+    } else {
+      console.error("retour du serveur : ", response.status);
     }
-
-  } else {
-    console.error("retour du serveur : ", response.status);
-  }
-}
-
-
-getTeddies();
-
-
-
-
-
-
-
-// il me faudra aussi en envoyer
+  }))
